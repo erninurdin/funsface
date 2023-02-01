5e94bc615eb8b73c0930c28c6dde88621627906a
erninurdin <erni5238e@gmail.com>
2023-02-01 13:21:42 +0800

    First Github Project

---
+++ app.py
@@ -0,0 +1,9 @@
+#hitung luas segitiga
+
+def luas_segitiga();
+a = int(input("Masukkan alas segitiga: "))
+t = int(input("Masukkan tiggi segitiga"))
+luas = a * t / 2
+print ("luas segitiga adalah: ",luas)
+
+luas_segitiga()
